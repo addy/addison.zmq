942ba8ad5c39f11c854f0ca7c2c298ac4dd7ab2f
Addison Shaw <addison.shaw@cerner.com>
2019-12-09 10:18:42 -0600

    Initial commit

---
+++ src/main/java/addison/addison/zmq/Server.java
@@ -0,0 +1,34 @@
+package addison.addison.zmq;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
+public class Server
+{
+   public static void main(String[] args)
+   {
+      try (ZContext context = new ZContext())
+      {
+         // Socket to talk to clients
+         ZMQ.Socket socket = context.createSocket(SocketType.REP);
+         socket.bind("tcp://*:5555");
+
+         while (!Thread.currentThread().isInterrupted())
+         {
+            byte[] reply = socket.recv(0);
+            System.out.println("Received " + ": [" + new String(reply, ZMQ.CHARSET) + "]");
+
+            String response = "world";
+            socket.send(response.getBytes(ZMQ.CHARSET), 0);
+
+            Thread.sleep(1000); // Do some 'work'
+         }
+      }
+      catch (InterruptedException e)
+      {
+         System.out.println("Couldn't sleep lol.");
+         e.printStackTrace();
+      }
+   }
+}
